3a293188760f9bb01e5969f198105efd5b5575ce
Abhishek Raj <abhisheksingh8253@gmail.com>
2021-07-20 16:19:14 +0530

    Add files via upload

---
+++ task1/js/min.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.gaming-slider').slick({
+        autoplay:true,
+        slidesToShow:3,
+        slidesToscroll:1,
+        prevArrow:".prev-btn",
+        nextArrow:".next-btn",
+        
+
+    });
+});
